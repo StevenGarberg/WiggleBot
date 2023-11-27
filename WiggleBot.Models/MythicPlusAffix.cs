d46c8adf057ab3b8af89ffbda2042410f4a361e3
Steven Garberg <steven.garberg.jr@gmail.com>
2023-11-27 18:19:50 -0500

    Added affixes page

---
+++ WiggleBot.Models/MythicPlusAffix.cs
@@ -0,0 +1,23 @@
+﻿namespace WiggleBot.Models;
+
+public record MythicPlusAffix(
+    AffixType Name,
+    string WowheadLink)
+{
+}
+
+public enum AffixType
+{
+    Fortified,
+    Tyrannical,
+    Incorporeal,
+    Entangling,
+    Volcanic,
+    Storming,
+    Afflicted,
+    Sanguine,
+    Bursting,
+    Spiteful,
+    Raging,
+    Bolstering
+}
---
+++ WiggleBot.Models/MythicPlusWeeklyAffixes.cs
@@ -0,0 +1,9 @@
+﻿namespace WiggleBot.Models;
+
+public record MythicPlusWeeklyAffixes(
+    byte WeekNumber,
+    AffixType Affix1,
+    AffixType Affix2,
+    AffixType Affix3)
+{
+}
